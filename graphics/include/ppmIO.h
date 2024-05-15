859f1017a573c129009501f261c7be7479db7893
dotafs2 <qdotafs@gmail.com>
2024-05-15 14:14:53 -0400

    init commit

--- graphics/include/ppmIO.h
+++ graphics/include/ppmIO.h
@@ -14,5 +14,4 @@ void writePPM(Pixel *image, int rows, int cols, int colors, char *filename);
 unsigned char *readPGM(int *rows, int *cols, int *intensities, char *filename);
 void writePGM(unsigned char *image, long rows, long cols, int intensities, char *filename);
 
-
 #endif
