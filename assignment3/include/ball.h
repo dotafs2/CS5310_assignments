b86d03d260f381edb95498151b6314106d07d96f
dotafs2 <qdotafs@gmail.com>
2024-05-27 16:35:53 -0400

    finish assignment3

---
+++ assignment3/include/ball.h
@@ -0,0 +1,21 @@
+//
+// Created by 曲辰熙 on 2024/5/27.
+//
+
+#ifndef ASSIGNMENT3_BALL_H
+#define ASSIGNMENT3_BALL_H
+
+#include <cstdio>
+#include <cstdlib>
+#include "Line.h"
+#include "Image.h"
+#include "anti-alias.h"
+#include <vector>
+using namespace std;
+
+
+vector<vector<Point>> init_vertex(int sliceX, int sliceY, Point center, float scale);
+vector<Triangle> init_triangles(vector<vector<Point>> vertex_set) ;
+void draw_ball(Image* src, int sliceX, int sliceY, Point center, float scale, Color c, bool fill);
+#endif //ASSIGNMENT3_BALL_H
+
